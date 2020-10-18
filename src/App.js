53435737163e25827ff02ffa5845f1e96fdd455a
minhbtc-0977 <57735738+minh2tc-0977@users.noreply.github.com>
2020-10-18 08:17:17 +0700

    Revert "Add demo week 1"

--- src/App.js
+++ src/App.js
@@ -1,100 +1,18 @@
 import React, { Component } from 'react';
-import Form from './components/Form';
-import Result from './components/Result';
+import logo from './logo.svg';
+import './App.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      user: {
-        name: '',
-        gender: '',
-        birthday: '',
-        self_intro: '',
-        interests: [],
-      },
-      languages: [
-        {
-          id: 1,
-          name: "VI"
-        },
-        {
-          id: 2,
-          name: "EN"
-        },
-        {
-          id: 3,
-          name: "JP"
-        },
-        { id: 4, name: "KR" }
-      ],
-      gender: [
-        {
-          id: 0,
-          name: 'Male',
-        },
-        {
-          id: 1,
-          name: 'Famale',
-        }
-      ],
-      interests: [
-        {
-          id: 1,
-          name: 'Money',
-        },
-        {
-          id: 2,
-          name: 'Girl friend',
-        },
-        {
-          id: 3,
-          name: 'All of the above',
-        }
-      ],
-      isSubmit: false,
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(e) {
-    const {name, value, type, checked} = e.target;
-    const prevUser = this.state.user;
-    this.setState({
-      user: {
-        ...prevUser,
-        [name]:
-          type === 'checkbox'
-            ? checked
-              ? [...prevUser.interests, JSON.parse(value)]
-              : prevUser.interests.filter((interest) => {
-                return interest.id !== JSON.parse(value).id;
-              })
-            : value
-      }
-    });
-  }
-
-  handleSubmit(e) {
-    e.preventDefault();
-    this.setState({
-      isSubmit: true
-    })
-  }
-
   render() {
     return (
       <div className="App">
-        <Form
-          gender={this.state.gender}
-          languages={this.state.languages}
-          interests={this.state.interests}
-          handleChange={this.handleChange}
-          handleSubmit={this.handleSubmit}
-        />
-        <Result isSubmit={this.state.isSubmit} user={this.state.user}/>
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
       </div>
     );
   }
